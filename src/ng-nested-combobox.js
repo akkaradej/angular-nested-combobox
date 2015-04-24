fffe1cb82390a797e974d3e90290ef61728c8473
akkaradej <akkaradej14@gmail.com>
2015-04-24 14:19:37 +0700

    can add placeholder to be first option

--- src/ng-nested-combobox.js
+++ src/ng-nested-combobox.js
@@ -10,6 +10,7 @@ angular.module('ui.nested.combobox', [])
         this.isOpen = false;
         this.currentMember = $scope.currentMember;
         this.filterBy = $scope.filterBy || {};
+        this.placeholder = $scope.placeholder;
 
         $scope.$watch('controlDisabled', function (value) {
             that.controlDisabled = value;
@@ -29,14 +30,12 @@ angular.module('ui.nested.combobox', [])
             if (oldMemberId === member.id) {
                 return true;
             }
-
-            if (member.id === 'root') {
-                member.name = event.currentTarget.innerText;
-            }
+            
             $scope.changeEvent(member);
             $scope.currentMember = that.currentMember = member;
             oldMemberId = member.id;
         };
+
     }])
     .directive('nestedComboBox', function () {
         return {
@@ -51,17 +50,24 @@ angular.module('ui.nested.combobox', [])
                 controlClass: '@',
                 controlDisabled: '@',
                 changeEvent: '=',
-                filterBy: '='
+                filterBy: '=',
+                placeholder: '@'
             }
         };
     })
     .run(['$templateCache', function($templateCache) {
         $templateCache.put('template/select-group.html',
         '<div class="custom-select"   data-ng-disabled="gs.controlDisabled==\'true\'" data-ng-class="controlClass" data-ng-click="gs.toggleOpen()">'+
-            '<p>{{gs.currentMember.name}}</p>'+
+            '<p>{{gs.currentMember.name || gs.placeholder}}</p>'+
             '<span><i class="icon-sort-down"></i></span>'+
             '<div class="list" data-ng-show="gs.isOpen">'+
                 '<ul>'+
+                    '<li data-ng-class="{\'active\':gs.currentMember === \'\'}" ng-init="member=\'\'" ng-if="gs.placeholder">'+
+                        '<div class="sub">'+
+                            '<div class="overlay"></div>'+
+                            '<a data-ng-click="gs.selectValue(e,member)"><span>{{gs.placeholder}}</span></a>'+
+                        '</div>'+
+                    '</li>'+
                     '<li data-ng-class="{\'active\':gs.currentMember.id === member.id}" data-ng-include="\'template/sub-level.html\'" data-ng-repeat="member in collection | filter: gs.filterBy"> </li>'+
                 '</ul>'+
             '</div>'+
